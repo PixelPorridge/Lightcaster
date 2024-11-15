209f5a5b144b6bfb4d5d67bddeb02c08b6061706
Torin <torinthearcher@gmail.com>
2024-11-16 00:50:57 +1100

    Added pause menu

--- Globals/GameManager.gd
+++ Globals/GameManager.gd
@@ -11,30 +11,56 @@ func _ready() -> void:
 	# Hide mouse cursor
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
+	# Game manager always runs
+	process_mode = ProcessMode.PROCESS_MODE_ALWAYS
+
 
 func _process(_delta: float) -> void:
 	# Close game on escape
 	if (Input.is_action_just_pressed(InputMapActions.CLOSE_GAME)):
 		get_tree().quit()
-	
+
 	# Toggle fullscreen
 	if (Input.is_action_just_pressed(InputMapActions.TOGGLE_FULLSCREEN)):
 		if (DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN):
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	
+
 	# Restart current level
 	if (Input.is_action_just_pressed(InputMapActions.RESTART_LEVEL)):
 		get_tree().reload_current_scene()
 
 
 func go_to_next_level():
+	# Do not attempt to load next level if maxed out
+	if (current_level >= levels.size() - 1):
+		get_tree().reload_current_scene()
+		return
+
 	# Increment current level
 	current_level += 1
 
-	# Do not attempt to load next level if maxed out
-	if (current_level >= levels.size()): return
-
 	# Change scene to next level
+	get_tree().change_scene_to_packed(levels[current_level])
+
+
+func go_to_previous_level():
+	# Do not attempt to load previous level if at start
+	if (current_level <= 0):
+		get_tree().reload_current_scene()
+		return
+
+	# Decrease current level
+	current_level -= 1
+
+	# Change scene to previous level
+	get_tree().change_scene_to_packed(levels[current_level])
+
+
+func restart_game():
+	# Reset current level
+	current_level = 0
+
+	# Go to start of game
 	get_tree().change_scene_to_packed(levels[current_level])
---
+++ Levels/PauseMenu/PauseMenu.gd
@@ -0,0 +1,74 @@
+class_name PauseMenu
+extends CanvasLayer
+
+@export var black: ColorRect
+@export var menu: Control
+
+
+func _process(_delta: float) -> void:
+	# Show or hide pause menu
+	if Input.is_action_just_pressed(InputMapActions.PAUSE_GAME):
+		if !get_tree().paused:
+			show_pause_menu()
+		else:
+			hide_pause_menu()
+
+
+func show_pause_menu():
+	# Pause game
+	get_tree().paused = true
+
+	# Show mouse cursor
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+	visible = true
+
+	black.modulate = Color(1, 1, 1, 0)
+	menu.modulate = Color(1, 1, 1, 0)
+	menu.position.y = 8
+
+	# Animate pause menu in
+	var tween = get_tree().current_scene.create_tween().bind_node(self).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT).set_parallel()
+
+	tween.tween_property(black, "modulate", Color(1, 1, 1, .5), .5)
+	tween.tween_property(menu, "modulate", Color(1, 1, 1, 1), .5)
+	tween.tween_property(menu, "position", Vector2(menu.position.x, 0), .5)
+
+
+func hide_pause_menu():
+	# Hide mouse cursor
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+	# Animate pause menu out
+	var tween = get_tree().current_scene.create_tween().bind_node(self).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT).set_parallel()
+
+	tween.tween_property(black, "modulate", Color(1, 1, 1, 0), .5)
+	tween.tween_property(menu, "modulate", Color(1, 1, 1, 0), .5)
+	tween.tween_property(menu, "position", Vector2(menu.position.x, 8), .5)
+
+	# Unpause game
+	get_tree().paused = false
+
+
+func _on_restart_level_pressed() -> void:
+	# Unpause game
+	get_tree().paused = false
+	
+	# Reload scene
+	get_tree().reload_current_scene()
+
+
+func _on_previous_level_pressed() -> void:
+	# Unpause game
+	get_tree().paused = false
+
+	# Go to previous level
+	GameManager.go_to_previous_level()
+
+
+func _on_restart_game_pressed() -> void:
+	# Unpause game
+	get_tree().paused = false
+
+	# Restart game
+	GameManager.restart_game()
